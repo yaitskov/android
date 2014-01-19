0a6688d7adbcf7eba6cb46ebbc2e98b0b9e04044
Daneel S. Yaitskov <rtfm.rtfm.rtfm@gmail.com>
2014-01-19 23:21:13 +0400

    log main activity status changes

--- src/main/java/com/github/app2/MainActivity.java
+++ src/main/java/com/github/app2/MainActivity.java
@@ -2,11 +2,46 @@ package com.github.app2;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends Activity {
+
+    public static final String MAIN = "MAIN";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        Log.d(MAIN, "create ----------------------------------------");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(MAIN, "resume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(MAIN, "stop");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(MAIN, "START");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(MAIN, "PAUSE");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(MAIN, "DESTROY -----------------------------------");
     }
 }
